7cfddb1ec5f65190dcf252ed327ef57364deb510
Ranapratap Ray <122826508+ranajay9399@users.noreply.github.com>
2023-07-12 10:45:18 +0530

    Update k8sss.sh

--- k8sss.sh
+++ k8sss.sh
@@ -8,6 +8,7 @@ apt install docker.io -y
 systemctl start docker
 systemctl enable docker
 
+Need to execute below commands as root user by doing "sudo su"
 curl -fsSL "https://packages.cloud.google.com/apt/doc/apt-key.gpg" | sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/kubernetes-archive-keyring.gpg
 echo 'deb https://packages.cloud.google.com/apt kubernetes-xenial main' > /etc/apt/sources.list.d/kubernetes.list
 
